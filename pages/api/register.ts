9622aff2353f9dc1a9860c77687b5e2223c12738
Govind4684 <46969712+Govind4684@users.noreply.github.com>
2024-08-29 13:58:25 +0530

    Re init

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -1,7 +1,6 @@
 import bcrypt from 'bcrypt';
 import { NextApiRequest,NextApiResponse } from 'next';
 import prismadb from '@/lib/prismadb';
-import { error } from 'console';
 
 export default async function handler(req:NextApiRequest,res:NextApiResponse) {
     if(req.method !== 'POST') {
